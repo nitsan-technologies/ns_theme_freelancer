8e7aac2d78f45df06f8abd215208dae73c96e23c
NITSAN <sanjay@nitsan.in>
2020-01-07 20:17:56 +0530

    [TODO] Clean-up old version

--- Configuration/PageTSconfig/setup.ts
+++
@@ -1,7 +0,0 @@
-# Include the BackendLayouts
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:ns_theme_freelancer/Configuration/PageTSconfig/BackendLayouts" extensions="ts">
-
-# Remove default custom elements from EXT:ns_basetheme 
-mod.wizards.newContentElement.wizardItems.extra {
-  show := removeFromList(ns_imageteaser, ns_record, ns_slider)
-}
--- Configuration/TypoScript/Extensions/Form/setup.ts
+++
@@ -1,9 +0,0 @@
-// Configure default frontend form
-plugin.tx_form.settings.yamlConfigurations {
-    100 = EXT:ns_theme_freelancer/Configuration/Form/setup.yaml
-}
-
-// Configure default backend form
-module.tx_form.settings.yamlConfigurations {
-    100 = EXT:ns_theme_freelancer/Configuration/Form/setup.yaml
-}
