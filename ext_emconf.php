4166f376f6267ce670dbd67013d7a591e3ab7f2a
NITSAN Technologies <sanjay@nitsan.in>
2020-06-13 14:47:42 +0530

    [TODO] Major Release v2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 // Provide detailed information and depenencies of EXT:ns_theme_freelancer
 $EM_CONF['ns_theme_freelancer'] = [
-    'title' => '[NITSAN] Freelancer TYPO3 Theme',
-    'description' => 'Every freelancer dream just came true, as we have made T3T freelancer! This beautiful TYPO3 theme is an excellent choice for any freelance agency and freelancer, as it enables them to showcase their work in a gorgeous manner. Demo: https://demo.t3terminal.com/t3t-freelancer/ PRO version: https://t3terminal.com/t3-freelancer-free/',
+    'title' => '[NITSAN] Freelancer TYPO3 Template',
+    'description' => 'Every freelancer dream just came true, as we have made T3T freelancer! This beautiful TYPO3 theme is an excellent choice for any freelance agency and freelancer, as it enables them to showcase their work in a gorgeous manner. Live-Demo: https://demo.t3terminal.com/?theme=t3t-freelancer PRO Support: https://t3terminal.com/t3-freelancer-free-typo3-portfolio-template',
     'category' => 'templates',
     'author' => 'T3:Milan Rathod, FE:Mehul Nimavat, QA:Vandna Makwana',
     'author_email' => 'info@nitsan.in',
