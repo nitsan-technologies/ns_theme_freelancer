c3b7fbb46dbd74de4e116b8aef611750e35f0625
Sonal Chauhan <sonal.nitsan@gmail.com>
2020-11-21 19:16:35 +0530

    Added the site configuration files

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 // Provide detailed information and depenencies of EXT:ns_theme_freelancer
 $EM_CONF['ns_theme_freelancer'] = [
-    'title' => '[NITSAN] Freelancer TYPO3 Template',
-    'description' => 'Every freelancer dream just came true, as we have made T3T freelancer! This beautiful TYPO3 theme is an excellent choice for any freelance agency and freelancer, as it enables them to showcase their work in a gorgeous manner. Live-Demo: https://demo.t3terminal.com/?theme=t3t-freelancer PRO Support: https://t3terminal.com/t3-freelancer-free-typo3-portfolio-template',
+    'title' => '[NITSAN] Freelancer TYPO3 Theme',
+    'description' => 'Every freelancer dream just came true, as we have made T3T freelancer! This beautiful TYPO3 theme is an excellent choice for any freelance agency and freelancer, as it enables them to showcase their work in a gorgeous manner. Demo: https://demo.t3terminal.com/t3t-freelancer/ PRO version: https://t3terminal.com/t3-freelancer-free/',
     'category' => 'templates',
     'author' => 'T3:Milan Rathod, FE:Mehul Nimavat, QA:Vandna Makwana',
     'author_email' => 'info@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
-    'version' => '2.0.0',
+    'version' => '2.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.0.0-10.9.99',
