032735bdaf4da45d54cbd72eaecaad9b72e97f6b
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2024-11-19 11:46:36 +0530

    [TASK] Update new icon & description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,17 +2,17 @@
 
 // Provide detailed information and depenencies of EXT:ns_theme_freelancer
 $EM_CONF['ns_theme_freelancer'] = [
-	'title' => '[NITSAN] T3 Freelancer TYPO3 Template',
-	'description' => 'Every freelancer dream just came true, as we have ve made T3 freelancer, Free TYPO3 Portfolio Template! Live-Demo: https://demo.t3terminal.com/?theme=t3t-freelancer PRO-version: https://t3terminal.com/t3-freelancer-free-typo3-portfolio-template',
+	'title' => 'T3 Freelancer TYPO3 Template',
+	'description' => 'Are you a TYPO3 freelancer? Are you looking for a free TYPO3 template for your portfolio? Create an eye-catching portfolio and showcase your skills with the T3 Freelancer Template. This template is perfect for illustrating your portfolio and allows you to create stylish presentations.',
 	'category' => 'templates',
 	'author' => 'Team NITSAN',
 	'author_email' => 'info@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
-	'version' => '13.0.0',
+	'version' => '13.0.1',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '13.0.0-13.4.99',
+			'typo3' => '12.0.0-13.4.99',
 			'ns_basetheme' => '12.0.0-13.4.99',
 		],
 		'conflicts' => [
