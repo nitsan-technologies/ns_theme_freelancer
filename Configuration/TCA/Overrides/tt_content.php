9ca19b3a49d44c9ca5e21fb9be27ac61b025c0e2
malu <malu.nitsan>
2025-09-04 12:44:08 +0530

    [TASK]Final Task

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,7 +5,7 @@ defined('TYPO3') or die();
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItemGroup(
     'tt_content', 
     'CType', 
-    'nsfreelancer_content_blocks', 
-    'LLL:EXT:ns_theme_freelancer/Resources/Private/Language/locallang.xlf:nsfreelancer_content_blocks', 
+    'nsfreelancer_blocks', 
+    'LLL:EXT:ns_theme_freelancer/Resources/Private/Language/locallang.xlf:nsfreelancer_blocks', 
     'before:default',
 );
